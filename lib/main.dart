97535aada0e32cafb4e1b3d720a863834ce6661b
Solongo1997 <solongos091@gmail.com>
2022-03-01 08:28:28 +0800

    morning

--- lib/main.dart
+++ lib/main.dart
@@ -3,36 +3,13 @@ import 'package:flutter/material.dart';
 void main() {
   runApp(const MyApp());
 }
+
 class MyApp extends StatelessWidget {
   const MyApp({Key? key}) : super(key: key);
-@override
-
-Widget build(BuildContext context){
-  return MaterialApp(
-    title: 'Flutter demo',
-    home: Scaffold(appBar: AppBar(
-      title: const Text('Flutter layout demo'),
-     ),
-     body: ListView(
-       children: const[
-         ListTile(
-           title: Text('ul hodloh'),
-           subtitle: Text('20000zar') ,
-           leading: Icon(Icons.home),
-         ),
-          ListTile(
-           title: Text('ul hodloh'),
-           subtitle: Text('20000zar'),
-           leading: Icon(Icons.home),
-         ),
-          ListTile(
-           title: Text('ul hodloh'),
-           subtitle: Text('20000zar') ,
-           leading: Icon(Icons.home),
-         ),
-       ],
-     ),
-   ),
-  );
-}
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      color: Colors.red,
+    );
+  }
 }
